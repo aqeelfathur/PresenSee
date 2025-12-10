a842b1db54d3f55b76a980a51c626574f37514ea
aqeelfathur <aqeelfathurrahman@gmail.com>
2025-12-10 19:02:52 +0700

    update sesi

--- PresenSee/resources/views/guru/sesi-presensi.blade.php
+++ PresenSee/resources/views/guru/sesi-presensi.blade.php
@@ -38,7 +38,6 @@
         </div>
     </div>
     
-    
     <!-- Selesai -->
     <div class="bg-white rounded-xl shadow-sm border border-gray-100 p-4">
         <div class="flex items-center justify-between">
