14a2f45d2acb055946c2970bb0c271279222b976
aqeelfathur <aqeelfathurrahman@gmail.com>
2025-12-13 12:45:24 +0700

    edit tabel user

--- PresenSee/database/migrations/2025_12_05_123903_create_siswa_table.php
+++ PresenSee/database/migrations/2025_12_05_123903_create_siswa_table.php
@@ -1,8 +1,10 @@
 <?php
 
+
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
+
 
 return new class extends Migration
 {
@@ -15,16 +17,14 @@ return new class extends Migration
             $table->id('id_siswa');
             $table->string('nis')->unique();
             $table->string('nama_siswa');
-            $table->string('no_handphone_siswa');
-            $table->string('email_siswa')->unique();
-            $table->date('tanggal_lahir_siswa');
             $table->string('nama_walimurid');
             $table->string('no_handphone_walimurid');
             $table->string('foto_siswa')->nullable();
-            $table->string('status');
+            $table->string('kelas');
             $table->timestamps();
         });
     }
+
 
     /**
      * Reverse the migrations.
