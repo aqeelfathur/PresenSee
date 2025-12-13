278259a103c09969f71d4e559382b0b0b0e3712f
aqeelfathur <aqeelfathurrahman@gmail.com>
2025-12-13 12:45:13 +0700

    fix nambah id_guru

--- PresenSee/database/seeders/SesiSeeder.php
+++ PresenSee/database/seeders/SesiSeeder.php
@@ -11,26 +11,26 @@ class SesiSeeder extends Seeder
     {
         // Buat beberapa sesi untuk contoh
         $sesi = [
-            // Matematika Kelas 7A (id_mapel_kelas = 1)
-            ['id_mapel_kelas' => 1, 'tanggal_sesi' => Carbon::now()->subDays(5)],
-            ['id_mapel_kelas' => 1, 'tanggal_sesi' => Carbon::now()->subDays(3)],
-            ['id_mapel_kelas' => 1, 'tanggal_sesi' => Carbon::now()],
+            // Matematika Kelas 7A (id_mapel_kelas = 1, guru = 2/Budi)
+            ['id_mapel_kelas' => 1, 'id_guru' => 2, 'tanggal_sesi' => Carbon::now()->subDays(5)],
+            ['id_mapel_kelas' => 1, 'id_guru' => 2, 'tanggal_sesi' => Carbon::now()->subDays(3)],
+            ['id_mapel_kelas' => 1, 'id_guru' => 2, 'tanggal_sesi' => Carbon::now()],
             
-            // B. Indonesia Kelas 7A (id_mapel_kelas = 2)
-            ['id_mapel_kelas' => 2, 'tanggal_sesi' => Carbon::now()->subDays(4)],
-            ['id_mapel_kelas' => 2, 'tanggal_sesi' => Carbon::now()->subDays(1)],
+            // B. Indonesia Kelas 7A (id_mapel_kelas = 2, guru = 3/Siti)
+            ['id_mapel_kelas' => 2, 'id_guru' => 3, 'tanggal_sesi' => Carbon::now()->subDays(4)],
+            ['id_mapel_kelas' => 2, 'id_guru' => 3, 'tanggal_sesi' => Carbon::now()->subDays(1)],
             
-            // B. Inggris Kelas 7A (id_mapel_kelas = 3)
-            ['id_mapel_kelas' => 3, 'tanggal_sesi' => Carbon::now()->subDays(6)],
-            ['id_mapel_kelas' => 3, 'tanggal_sesi' => Carbon::now()->subDays(2)],
+            // B. Inggris Kelas 7A (id_mapel_kelas = 3, guru = 6/Rudi)
+            ['id_mapel_kelas' => 3, 'id_guru' => 6, 'tanggal_sesi' => Carbon::now()->subDays(6)],
+            ['id_mapel_kelas' => 3, 'id_guru' => 6, 'tanggal_sesi' => Carbon::now()->subDays(2)],
             
-            // IPA Kelas 7A (id_mapel_kelas = 4)
-            ['id_mapel_kelas' => 4, 'tanggal_sesi' => Carbon::now()->subDays(5)],
-            ['id_mapel_kelas' => 4, 'tanggal_sesi' => Carbon::now()],
+            // IPA Kelas 7A (id_mapel_kelas = 4, guru = 4/Ahmad)
+            ['id_mapel_kelas' => 4, 'id_guru' => 4, 'tanggal_sesi' => Carbon::now()->subDays(5)],
+            ['id_mapel_kelas' => 4, 'id_guru' => 4, 'tanggal_sesi' => Carbon::now()],
             
-            // Matematika Kelas 8A (id_mapel_kelas = 9)
-            ['id_mapel_kelas' => 9, 'tanggal_sesi' => Carbon::now()->subDays(4)],
-            ['id_mapel_kelas' => 9, 'tanggal_sesi' => Carbon::now()->subDays(1)],
+            // Matematika Kelas 8A (id_mapel_kelas = 9, guru = 2/Budi)
+            ['id_mapel_kelas' => 9, 'id_guru' => 2, 'tanggal_sesi' => Carbon::now()->subDays(4)],
+            ['id_mapel_kelas' => 9, 'id_guru' => 2, 'tanggal_sesi' => Carbon::now()->subDays(1)],
         ];
 
         foreach ($sesi as $s) {
